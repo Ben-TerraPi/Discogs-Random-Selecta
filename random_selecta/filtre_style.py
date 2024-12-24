e646dbf5b40a6cd965442e727271333f8c496423
Benoit Dourdet <benoit.dourdet@gmail.com>
2024-12-24 18:47:51 +0000

    v1.3

--- random_selecta/filtre_style.py
+++ random_selecta/filtre_style.py
@@ -2,6 +2,6 @@
 import pandas as pd
 
 #%%
-df = pd.read_csv("tableau_genre.csv")
-df = df.drop(columns=['Unnamed: 0'])
-df
+# df = pd.read_csv("tableau_genre.csv")
+# df = df.drop(columns=['Unnamed: 0'])
+# df
