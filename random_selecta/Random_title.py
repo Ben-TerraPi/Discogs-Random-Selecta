29c218db414659031fb2a039db515ace809ca25a
Benoit Dourdet <benoit.dourdet@gmail.com>
2024-12-24 19:45:08 +0000

    test3

--- random_selecta/Random_title.py
+++ random_selecta/Random_title.py
@@ -6,7 +6,7 @@ import streamlit as st
 import discogs_client
 from datetime import datetime
 from list_styles import electro_style, rock_style, pop_list, funk_soul_style, jazz_style, world_style,classical_style, hip_hop_style, stage_style, latin_style, reggae_style, blues_style, non_music_style, children_style, military_style, genres_styles
-# from utils import token, random_selecta
+from utils import token, random_selecta
 
 # token = "FFCHhXOkwaGQGbBGejcGGYsdUngQcVZjbijuCVrZ"
 
