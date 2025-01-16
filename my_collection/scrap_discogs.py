727018dfbd4cab29c21b551799afb75a2ae7273b
Benoît Dourdet <benoit.dourdet@gmail.com>
2025-01-16 22:53:14 +0100

    change on scrap_

--- my_collection/scrap_discogs.py
+++ my_collection/scrap_discogs.py
@@ -5,31 +5,31 @@ import discogs_client
 import operator
 import csv
 import pprint
+import streamlit as st
 
 
 #Discogs Client & User token
 
 token = st.secrets["token"]["user_token"]
-
 d = discogs_client.Client("ExampleApplication/0.1", user_token= token)
+
+
 #Mon compte
 
 me = d.identity()
+print(dir(me))
 
 print(me.id)
 print(me.location)
 print(me.name)
 print(me.url)
-#print(me.collection_folders)
 
-print(dir(me))
 
 #list collection
 data = []
 for item in me.collection_folders[0].releases:
       data.append(item)
 
-#data
 
 #Import de ma Collection
 
