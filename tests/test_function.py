4292bf212cf9cb864379f6377940eb28afffa2ad
Benoit Dourdet <benoit.dourdet@gmail.com>
2025-01-17 14:35:41 +0100

    Update test_function.py

--- tests/test_function.py
+++ tests/test_function.py
@@ -8,7 +8,7 @@ token = st.secrets["token"]["user_token"]
 
 d = discogs_client.Client("ExampleApplication/0.1", user_token= token)
 
-#>>>>>>>>>>>>>>>>>>>>>>>> FUNCTION
+#>>>>>>>>>>>>>>>>>>>>>>>> FUNCTIONS
 
 def list_albums(genre, year):
     list = []
@@ -18,4 +18,19 @@ def list_albums(genre, year):
     return pd.DataFrame(list)
    
 # %%
-list_albums("Hip Hop",1983) 
+list_albums("Hip Hop",1986) 
+
+#%%
+def random_album(genre, year):
+    results = d.search(genre=genre,year=year)
+    test = len(results)
+    if test != 0:
+        page_random = random.randint(1,results.pages)
+        nb_results = len(results.page(page_random))
+        k_random = random.randint(0,nb_results-1)
+        return results.page(page_random)[k_random]
+    else:
+        return "todo"
+
+#%%
+random_album("hip hop", 1986)
