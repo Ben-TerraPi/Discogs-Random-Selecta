90ccf45de2e55497de8a6c74b2887193e99c3178
Benoît Dourdet <benoit.dourdet@gmail.com>
2025-07-20 17:40:16 +0200

    amélioration de la recherche youtube

--- random_selecta/utils.py
+++ random_selecta/utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import random
 import discogs_client
 import streamlit as st
+import re
 from googleapiclient.discovery import build
 
 #Discogs Client & User token
@@ -69,6 +70,17 @@ def random_selecta(genre,style, year):
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> randm selecta + youtube
 
+def clean_string(s):
+    # Nettoie la chaîne
+    return re.sub(r'[^a-z0-9 ]', '', s.lower())
+
+def artist_and_title_in_video(video_title, artist, title):
+    vt = clean_string(video_title)
+    return clean_string(artist) in vt and clean_string(title) in vt
+
+def artist_in_video(video_title, artist):
+    return clean_string(artist) in clean_string(video_title)
+
 
 def random_youtube(genre, style= None, year= None):
     # results = d.search(genre=genre, style=style, year=year)
@@ -104,17 +116,18 @@ def random_youtube(genre, style= None, year= None):
 
             # Info album
             title = album.title
+            artist = album.artists[0].name if hasattr(album, 'artists') and album.artists else ""
             if hasattr(album, 'images') and album.images:
                 image = album.images[0]["uri"]
             link = album.url
 
-            # Recherche Youtube
+            # Recherche Youtube (lien cliquable)
             str = title.lower()
             str2 = str.replace(" ", "+")
             str3 = str2.replace("&", "and")
             url = f'https://www.youtube.com/results?search_query={str3}+audio'
 
-            # Vidéo Discogs
+            # Vidéo Discogs par défaut
             if hasattr(album, 'videos') and album.videos:
                 nb = len(album.videos)
                 if nb > 0:
@@ -122,26 +135,40 @@ def random_youtube(genre, style= None, year= None):
                     discogs_videos = album.videos[key_random].url
             else:
                 # API YouTube
+                query = f"{artist} - {title} audio"
                 youtube = build('youtube', 'v3', developerKey=api_key)
                 request = youtube.search().list(
                     part="snippet",
-                    q=str3,
+                    q=query,
                     type="video",
-                    maxResults=1
+                    maxResults=7,
+                    order="relevance"
                 )
                 try:
                     response = request.execute()
                 except:
                     response = None
+                
+                youtube_results = []
+                best_artist_and_title = None
+                best_artist = None
 
                 # Résultats recherche YouTube
                 if response:
-                    youtube_results = []
                     for item in response['items']:
                         video_id = item['id']['videoId']
                         video_title = item['snippet']['title']
                         video_url = f"https://www.youtube.com/watch?v={video_id}"
                         youtube_results.append({'title': video_title, 'url': video_url})
+                        if not best_artist_and_title and artist_and_title_in_video(video_title, artist, title):
+                            best_artist_and_title = {'title': video_title, 'url': video_url}
+                        elif not best_artist and artist_in_video(video_title, artist):
+                            best_artist = {'title': video_title, 'url': video_url} 
+
+                if best_artist_and_title:
+                    youtube_results = [best_artist_and_title] + [res for res in youtube_results if res != best_artist_and_title]
+                elif best_artist:
+                    youtube_results = [best_artist] + [res for res in youtube_results if res != best_artist]                     
 
     return title, image, url, link, discogs_videos, youtube_results, test
 
